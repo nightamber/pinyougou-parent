c28b711bef221d76fd16955bccc52d2c620a98d4
nightamber <nightelfamber@gmail.com>
2018-05-14 07:53:06 +0800

    增加了商品规格 等controlller

---
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandContrloler.js
@@ -0,0 +1,79 @@
+
+app.controller('brandController',function ($scope, $http,$controller,brandService) {
+
+    $controller('baseController',{$scope:$scope});
+
+    $scope.findAll = function () {
+
+        brandService.findAll().success(
+            function (response) {
+                $scope.list=response;
+            }
+        );
+
+    }
+
+
+
+    $scope.findPage=function (page,size) {
+        brandService.findPage(page,size).success(function (response) {
+            $scope.list=response.rows;
+            $scope.paginationConf.totalItems=response.total;
+        });
+    }
+
+    $scope.save=function () {
+        var object = null;
+
+        if($scope.entity.id != null){
+            object = brandService.update($scope.entity)
+        }else{
+            object = brandService.add($scope.entity)
+        }
+
+        object.success(
+            function (response) {
+                if(response.success){
+                    $scope.reloadList();
+                }else {
+                    alert(response.message);
+                }
+            }
+        )
+    }
+
+    $scope.findOne=function (id) {
+        brandService.findOne(id).success(
+            function (response) {
+                $scope.entity=response;
+            }
+        )
+    }
+
+
+
+    $scope.dele=function () {
+        if(confirm('确定要删除吗？')){
+            brandService.dele($scope.selectIds).success(
+                function(response){
+                    if(response.success){
+                        $scope.reloadList();//刷新
+                    }else{
+                        alert(response.message);
+                    }
+                }
+            );
+        }
+    }
+
+    $scope.searchEntity={};
+    $scope.search=function (page,size) {
+        brandService.search(page,size,$scope.searchEntity)
+            .success(function (response) {
+                $scope.paginationConf.totalItems=response.total;
+                $scope.list=response.rows;
+            })
+    }
+
+
+});
