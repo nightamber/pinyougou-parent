f632bcae8106058b6ffe974f9075266cd88abeb4
nightamber <nightelfamber@gmail.com>
2018-05-28 21:11:48 +0800

    完成5月28日任务

--- pinyougou-page-interface/src/main/java/com/pinyougou/page/service/ItemPageService.java
+++ pinyougou-page-interface/src/main/java/com/pinyougou/page/service/ItemPageService.java
@@ -7,4 +7,13 @@ public interface ItemPageService {
      * @return
      */
     public boolean genItemHtml(Long goodsId);
+
+    /**
+     *删除商品详细页
+     *
+     *
+     * @param goodsIds
+     * @return
+     */
+    boolean deleteItemHtml(Long[] goodsIds);
 }
